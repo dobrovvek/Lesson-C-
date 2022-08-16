13055555c1bb24d8a9fa3e0948f119d25715ade8
Dobrovvek <dobrovvek@gmail.com>
2022-08-16 21:20:26 +0300

    if and else

---
+++ exemple005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("User Name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
